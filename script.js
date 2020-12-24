72c8c9ee2709277e4f637283312a21c0a3e4a21c
Brandon D <brando2147@gmail.com>
2020-12-23 19:39:04 -0700

    Updating which data to pull from the API

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
 
+let apiKey = "a4ae7310bb584baaafb75d7ff837949e"
+let ingredientsArr = [];
+let ingredients = "sugar,+flour,"
+let resultsQuantity = 50;
+let queryURL = "https://api.spoonacular.com/recipes/findByIngredients?" +
+    "apiKey=" +
+    apiKey +
+    "&ingredients=" +
+    ingredients +
+    "&number=" +
+    resultsQuantity;
 
-apiKey = "a4ae7310bb584baaafb75d7ff837949e"
-// ingredientsArr = [];
-// resultsQuantity = 
 
-// querUrl = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" + ingredient  + resultsQuantity + apiKey 
-queryURL = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=2"
 
 $.ajax({
     url: queryURL,
     method: "GET"
-    apiKey: a4ae7310bb584baaafb75d7ff837949e
 }).then(function (response) {
 
-
     console.log(response)
+    console.log(response[0].missedIngredients[0].image)
+    console.log(response[0].missedIngredients[0].name)
+    console.log(response[0].missedIngredients[0].amount)
+    console.log(response[0].missedIngredients[0].unit)
+    console.log(response[0].missedIngredients[0].unitLong)
+    console.log(response[0].missedIngredients[0].unitShort)
+    console.log(response[0].missedIngredients[0].metaInformation[0])
+    console.log(response[0].missedIngredients[0].original)
+    console.log(response[0].missedIngredients[0].originalName)
+    console.log(response[0].missedIngredients[0].originalString)
 
 
 });
 
+
+
+// .ajax({
+//     url: queryURL,
+//     method: "GET"
+// }).then(function (response) {
