fa7ad9701233516a7a8b51c634eee648c987c185
Brandon D <brando2147@gmail.com>
2020-12-23 21:15:18 -0700

    Added additional API call

--- script.js
+++ script.js
@@ -12,30 +12,51 @@ let queryURL = "https://api.spoonacular.com/recipes/findByIngredients?" +
     resultsQuantity;
 
 
-
 $.ajax({
     url: queryURL,
     method: "GET"
+
 }).then(function (response) {
 
     console.log(response)
-    console.log(response[0].missedIngredients[0].image)
-    console.log(response[0].missedIngredients[0].name)
-    console.log(response[0].missedIngredients[0].amount)
-    console.log(response[0].missedIngredients[0].unit)
-    console.log(response[0].missedIngredients[0].unitLong)
-    console.log(response[0].missedIngredients[0].unitShort)
-    console.log(response[0].missedIngredients[0].metaInformation[0])
-    console.log(response[0].missedIngredients[0].original)
-    console.log(response[0].missedIngredients[0].originalName)
-    console.log(response[0].missedIngredients[0].originalString)
+    console.log("Image: " + response[0].missedIngredients[0].image)
+    console.log("Name: " + response[0].missedIngredients[0].name)
+    console.log("Amount: " + response[0].missedIngredients[0].amount)
+    console.log("Unit: " + response[0].missedIngredients[0].unit)
+    console.log("Unit Long: " + response[0].missedIngredients[0].unitLong)
+    console.log("Unit Short: " + response[0].missedIngredients[0].unitShort)
+    console.log("Meta: " + response[0].missedIngredients[0].metaInformation[0])
+    console.log("Original: " + response[0].missedIngredients[0].original)
+    console.log("Original Name: " + response[0].missedIngredients[0].originalName)
+    console.log("Original String: " + response[0].missedIngredients[0].originalString)
 
 
 });
 
 
 
-// .ajax({
-//     url: queryURL,
-//     method: "GET"
-// }).then(function (response) {
+let recipeId = 716429;
+let includeNutrition = false;
+let queryURL2 = "https://api.spoonacular.com/recipes/" +
+    "?apiKey=" +
+    apiKey +
+    recipeId +
+    "/" +
+    "&information" +
+    "?includeNutrition=" +
+    includeNutrition;
+
+
+$.ajax({
+    url: queryURL2,
+    method: "GET"
+}).then(function (response2) {
+
+
+    console.log(response2)
+
+
+});
+
+
+
