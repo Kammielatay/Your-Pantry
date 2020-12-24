9e44cc0390092b1973aba24a149e6087311913ed
Kameshia Ingram <Kameshialatay@gmail.com>
2020-12-24 12:32:45 -0700

    fixing responsiveness

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-let ingredientsArray = ['Apples', 'Avocado', 'Bacon', 'Baking Powder', 'Barbecue Sauce', 'Bread', 'Broccoli', 'Dijon Mustard', 'Brown Sugar', 'Buffalo/Hot Sauce', 'Butter/ Margarine','Cauliflower' , 'Cayenne Pepper', 'Cheddar Cheese', 'Cheese (in General)', 'Chicken', 'Turkey', 'Chicken Broth', 'Cinnamon', 'Cream Cheese', 'Cream of Veggie', 'Panko', 'Eggs', 'Fish', 'Flour', 'Fruit', 'Garlic', 'Garlic Powder', 'Green Onions', 'Green Peppers', 'Ground Beef', 'Honey', 'Ketchup', 'Lemons', 'Mayonnaise', 'Milk', 'Mushrooms', 'Mustard', 'Olive Oil', 'Onions/Shallots', 'Pancake /Baking Mix', "Pasta Noodles", 'Peanut Butter', 'Peppers', 'Pork Chops', 'Potatoes', 'Ramen', 'Rice', 'Rolls/Biscuits', 'Salsa', 'Sausage', 'Shortening', 'Tomato/Red Sauce', 'Tomatoes', 'Tortillas/Taco Shells', 'Vanilla Extract', 'Vegetables', 'Vinegar - Balsamic', 'Yellow Cake Mix', 'Zucchini']
+let ingredientsArray = ['Apples', 'Avocado', 'Bacon', 'Baking Powder', 'Barbecue Sauce', 'Bread', 'Broccoli', 'Dijon Mustard', 'Brown Sugar', 'Buffalo/Hot Sauce', 'Butter/ Margarine','Cauliflower' , 'Cayenne Pepper', 'Cheddar Cheese', 'Cheese (in General)', 'Chicken', 'Turkey', 'Chicken Broth', 'Cinnamon', 'Cream Cheese', 'Cream of Veggie', 'Panko', 'Eggs', 'Fish', 'Flour', 'Fruit', 'Garlic', 'Garlic Powder', 'Green Onions', 'Green Peppers', 'Ground Beef', 'Honey', 'Ketchup', 'Lemons', 'Mayonnaise', 'Milk', 'Mushrooms', 'Mustard', 'Olive Oil', 'Onions/Shallots', 'Pancake /Baking Mix', "Pasta Noodles", 'Peanut Butter', 'Peppers', 'Pork Chops', 'Potatoes', 'Ramen', 'Rice', 'Rolls/Biscuits', 'Salsa', 'Sausage', 'Shortening', 'Tomato/Red Sauce', 'Tomatoes', 'Tortillas/Taco Shells', 'Vanilla Extract', 'Vegetables', 'Vinegar - Balsamic', 'Yellow Cake Mix', 'Zucchini'];
 
-function generateBtn (){
-     let ingredientsbtn = $('<button>Get Recipe!</button>');
-    ingredientsbtn.addClass('generate-recipe waves-effect waves-light btn center-align')
-    $('.container').append(ingredientsbtn)
-}
-
-$('#click-here').on('click', function (){
-
+// Array of selected ingredients from user
+let selectedIngredients = [];
+ 
+// scrolls the page down to respective div
+function scrollPage() {
     $('html, body').animate({
         scrollTop: $("#checkbox").offset().top
     });
+}
+
+// function to display the checkboxes on the screen 
+function displayCheckboxes() {
+    $('#checkbox').text('');
 
     for (let i = 0; i < ingredientsArray.length; i++){
         let pContainer = $("<p class=label>");
@@ -19,7 +21,7 @@ $('#click-here').on('click', function (){
         let spanContainer = $('<span>');
         
         pContainer.addClass('col s2')
-        inputContainer.addClass('filled-in');
+        inputContainer.addClass('filled-in checked="unchecked"');
         spanContainer.text(ingredientsArray[i]);
         inputContainer.attr('value', ingredientsArray[i]);
 
@@ -29,8 +31,34 @@ $('#click-here').on('click', function (){
         labelContainer.append(spanContainer);
         
     }
+}
 
-    generateBtn()
+// takes the selective values from checkbox and pushes it to an array
+function selectedItems() {
+    $('input').on('click', function(){
+        console.log($(this).val())
+        var checked = ($(this).val());
+        selectedIngredients.push(checked)
+    })
+}
+
+// creates a button that will will later be clicked to grab recipes
+function generateBtn() {
+
+    let ingredientsbtn = $('<button>Get Recipe!</button>');
+    ingredientsbtn.addClass('generate-recipe waves-effect waves-light btn center-align')
+    $('.container').append(ingredientsbtn)
+}
+
+// when the user clicks the start button, these functions will run
+$('#start-here').on('click', function (){
+
+    scrollPage();
+    displayCheckboxes();
+    selectedItems();
+    generateBtn();
    
 })
 
+
+console.log(selectedIngredients)
