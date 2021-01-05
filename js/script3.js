218cbb169396751f164cd487bd85d8563c424136
Kameshia Ingram <kammie@Kameshias-Air.domain>
2021-01-05 10:31:04 -0700

    working on second api usage

---
+++ js/script3.js
@@ -0,0 +1,9 @@
+let UPC = "3600029145"
+let apiKey = "7cd0775fce29a8241d68e23b887d8651";
+let appID = "ed1ba166";
+let queryURL = 'https://api.edamam.com/api/food-database/v2/parser?upc='+
+UPC +
+'&app_id='+
+appID+
+'&app_key='+
+apiKey;
